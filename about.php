7548cb963b7b2cea1fd9525e2c0709588c29356f
Aoki0831 <aotaro27@gmail.com>
2019-02-23 18:34:34 +0900

    ほとんでできあ＼た

--- about.php
+++ about.php
@@ -1,21 +1,26 @@
 <?php
 
-// ヘッダーを読み込む
-include "./head.php";
+// head要素を読み込む
+include_once("./head.php");
 $title = about;
 
 
 ?>
 <!-- aboutページです -->
-
-<?= require_once("./header.html");?>
+<!-- header読み込み -->
+<?= include_once("./header.html");?>
 
 <article　class="aboutArticle">
+    <h1 class="aboutArticle__title">当サイトについて</h1>
     <section class="aboutArticle__section">
-        <h1 class="aboutArticle__section--title">当サイトについて</h1>
-        <p class="aboutArticle__section--content">アウトドア好きが送る、このサイトを見たらすぐにでも行きたくなるような魅力的なキャンプ場の数々を紹介するサイトです</p>
+        <p class="aboutArticle__section--content">アウトドア好きが送る、このサイトを見たらすぐにでも行きたくなるような魅力的なキャンプ場の数々を紹介するサイトです！　Let's
+            キャンプ!!
+        </p>
 
+        <div class="aboutArticle__photo"></div>
     </section>
 </article>
 
-<?= require_once("./footer.html");
+
+<!-- footer読み込み -->
+<?= include_once("./footer.html");
