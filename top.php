4005266fda4b3fb89075514c6df50ec1a70d6167
Aoki0831 <aotaro27@gmail.com>
2019-02-23 16:16:13 +0900

    topページ開発

---
+++ top.php
@@ -0,0 +1,24 @@
+<!-- トップページ -->
+<?php
+// ヘッダーを読み込む
+
+?>
+<html>
+
+<head>
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="other.css">
+</head>
+
+<body>
+
+
+    <main class="topPage">
+        <div class="topPage__photo">
+            <p class="topPage__title">タイトル</p>
+            <a href="index.php" class="topPage__link">読んでみる</a>
+        </div>
+    </main>
+</body>
+
+</html>
