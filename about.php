cf660684573cfedc898843fc469002269356362c
Aoki0831 <aotaro27@gmail.com>
2019-02-23 17:05:59 +0900

    簡単だけどheader,footer完成

--- about.php
+++ about.php
@@ -1,6 +1,17 @@
 <?php
 
 // ヘッダーを読み込む
+require_once("./head.php");
+require_once("./header.html");
+$title = about
 
 ?>
 <!-- aboutページです -->
+
+<article　class="aboutArticle">
+    <section class="aboutArticle__section">
+        <h1 class="aboutArticle__section--title">当サイトについて</h1>
+        <p class="aboutArticle__section--content">アウトドア好きが送る、このサイトを見たらすぐにでも行きたくなるような魅力的なキャンプ場の数々を紹介するサイトです</p>
+
+    </section>
+</article>
---
+++ haed.php
@@ -0,0 +1,19 @@
+<!-- htmlのhead部分のテンプレート -->
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <title><?= $title?>
+  </title>
+  <link rel="stylesheet" href="reset.css" />
+  <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="other.css" />
+</head>
+
+<body></body>
+
+</html>
