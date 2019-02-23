15057142732cb6574fccfccc7d83c49009540ab5
unknown <ema.tomo9821@docomo.ne.jp>
2019-02-23 17:24:45 +0900

    htmlが終了しCSSに突入

--- index.php
+++ index.php
@@ -5,10 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>Document</title>
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="flont.css">
   </head>
   <body>
     <article class="article">
-      <h1>おすすめのキャンプ場10選</h1>
+      <h1>おすすめのキャンプ場紹介</h1>
       <section class="article__section">
         <strong class="article__link1--section">PIKA富士吉田</strong>
         <p>
@@ -31,22 +34,23 @@
         >
       </section>
       <section class="article__section">
-        <strong class="article__link1--section">PIKA富士吉田</strong>
+        <strong class="article__link1--section">グランピング×農業体験「THE FARM」</strong>
         <p>
-          なんといっても充実した充宿泊施設!!グランピングキャンプから普通のキャンプまですべて体感できる。なかでも注目は1泊2食材付きの「テントヴィラ」。豪華テントと食材が提供され、贅沢なアウトドア気分が味わえます。
+          ゴージャスなグランピングに、農業体験まで出来るキャンプ場。自然を体験したい人や家族ずれには大人気のキャンプです。
+
         </p>
         <a
-          href="https://www.google.com/search?rlz=1C1EJFA_enJP695JP706&ei=lfFwXIOFM4yj8QWN3pKIBw&q=%E7%99%BD%E5%B4%8E%E6%B5%B7%E6%B4%8B%E5%85%AC%E5%9C%92%E3%80%80&oq=%E7%99%BD%E5%B4%8E%E6%B5%B7%E6%B4%8B%E5%85%AC%E5%9C%92%E3%80%80&gs_l=psy-ab.3..0l8.1515.1613..2273...0.0..0.125.232.0j2......0....1..gws-wiz.pAkrEs0dQyM"
-          >https://www.google.com/search?rlz=1C1EJFA_enJP695JP706&ei=lfFwXIOFM4yj8QWN3pKIBw&q=%E7%99%BD%E5%B4%8E%E6%B5%B7%E6%B4%8B%E5%85%AC%E5%9C%92%E3%80%80&oq=%E7%99%BD%E5%B4%8E%E6%B5%B7%E6%B4%8B%E5%85%AC%E5%9C%92%E3%80%80&gs_l=psy-ab.3..0l8.1515.1613..2273...0.0..0.125.232.0j2......0....1..gws-wiz.pAkrEs0dQyM</a
+          href="https://www.thefarm.jp/"
+          >https://www.thefarm.jp/</a
         >
       </section>
       <section class="article__section">
-        <strong class="article__link1--section">PIKA富士吉田</strong>
+        <strong class="article__link1--section">若杉高原おおやキャンプ場</strong>
         <p>
-          なんといっても充実した充宿泊施設!!グランピングキャンプから普通のキャンプまですべて体感できる。なかでも注目は1泊2食材付きの「テントヴィラ」。豪華テントと食材が提供され、贅沢なアウトドア気分が味わえます。
+          星空を見るならココ!!かつて「星がきれいにみえるまち全国1位」にも輝いたことがある。冬はスキー場になり、夏場はソリなどもして遊べるので小さいお子さんがいるご家庭には最適です。
         </p>
-        <a href="http://yoshida.pica-village.jp/"
-          >http://yoshida.pica-village.jp/</a
+        <a href="http://www.ooya-ski.jp/stay1.html"
+          >http://www.ooya-ski.jp/stay1.html</a
         >
       </section>
     </article>
